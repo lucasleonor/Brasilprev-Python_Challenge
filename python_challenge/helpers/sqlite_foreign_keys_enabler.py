1300b058d65d72d76758b377da2e7e825f1c9e76
lgleonor <lgleonor@marketdata.com.br>
2020-07-10 21:22:25 -0300

    Enabled foreing keys for sqlite3

---
+++ python_challenge/helpers/sqlite_foreign_keys_enabler.py
@@ -0,0 +1,10 @@
+from sqlalchemy.engine import Engine
+
+from python_challenge.setup.setup_db import db
+
+
+@db.event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
--- python_challenge/setup/setup_db.py
+++ python_challenge/setup/setup_db.py
@@ -10,6 +10,7 @@ def setup_db(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 
     db.init_app(app)
+    from python_challenge.helpers import sqlite_foreign_keys_enabler
 
     with app.app_context():
         db.create_all()
